5e8816aeef34b408f4d29e0f7345891b638e993c
Leighton <l.k.buchanancates@gmail.com>
2024-01-08 15:52:14 -0500

    adds border-radius to images, deletes background colors for carousel containers

--- slider.js
+++ slider.js
@@ -1,35 +1,28 @@
-const track = document.querySelector(".carousel__track");
-const slides = Array.from(track.children);
-const leftButton = document.querySelector(".carousel__button--left");
-const rightButton = document.querySelector(".carousel__button--right");
-const slideWidth = slides[0].getBoundingClientRect().width / 2;
+const carouselImages = document.querySelectorAll(".carousel__container img");
 
-console.log(slideWidth);
-
-// arrange slides next to each other
-const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWidth * index + "px";
-};
-slides.forEach(setSlidePosition);
-
-//function
-const moveToSlide = (track, currentSlide, targetSlide) => {
-  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
-  currentSlide.classList.remove("current-slide");
-  targetSlide.classList.add("current-slide");
-};
-
-// when click left, move slides to left
-leftButton.addEventListener("click", (e) => {
-  const currentSlide = track.querySelector(".current-slide");
-  const prevSlide = currentSlide.previousElementSibling;
-  moveToSlide(track, currentSlide, prevSlide);
+carouselImages.forEach((img, idx) => {
+  img.style.transform = `translateX(${idx * 100}%)`;
 });
 
-// when click right, move slides to right
-rightButton.addEventListener("click", (e) => {
-  const currentSlide = track.querySelector(".current-slide");
-  const nextSlide = currentSlide.nextElementSibling;
-  // move to the next slide
-  moveToSlide(track, currentSlide, nextSlide);
+let curSlide = 0;
+let maxSlide = carouselImages.length - 1;
+const leftArrow = document.querySelector(".carousel__button--left");
+const rightArrow = document.querySelector(".carousel__button--right");
+
+leftArrow.addEventListener("click", function () {
+  curSlide--;
+  carouselImages.forEach((img, idx) => {
+    img.style.transform = `translateX(${100 * (idx - curSlide)}%)`;
+  });
+});
+
+rightArrow.addEventListener("click", function () {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  carouselImages.forEach((img, idx) => {
+    img.style.transform = `translateX(${100 * (idx - curSlide)}%)`;
+  });
 });
