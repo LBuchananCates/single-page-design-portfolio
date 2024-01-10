9830cb8462c1d556bb9943a3e47538a33af11ad8
Leighton <l.k.buchanancates@gmail.com>
2024-01-10 17:32:39 -0500

    adds left arrow functionality for photo slider

--- slider.js
+++ slider.js
@@ -7,18 +7,26 @@ carouselImages.forEach((img, idx) => {
 
 let curSlide = 0;
 let maxSlide = carouselImages.length - 1;
+let minSlide = carouselImages.length - carouselImages.length;
 const leftArrow = document.querySelector(".carousel__button--left");
 const rightArrow = document.querySelector(".carousel__button--right");
 
+// left arrow
 leftArrow.addEventListener("click", function () {
-  curSlide--;
+  if (curSlide === minSlide) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
   carouselImages.forEach((img, idx) => {
     let gap = idx * 5;
     img.style.transform = `translateX(${100 * (idx - curSlide) + gap}%)`;
   });
 });
 
+// right arrow
 rightArrow.addEventListener("click", function () {
+  // if current slide is at final slide, restart, otherwise, add one
   if (curSlide === maxSlide) {
     curSlide = 0;
   } else {
@@ -30,6 +38,7 @@ rightArrow.addEventListener("click", function () {
   });
 });
 
-// 1. center first image
-// 2. active slide should be centered
+const centeredImg = window.innerWidth / 2;
+console.log(centeredImg);
+// 1. center first image: click left or right, style curSlide as centered in viewport
 // 5. ensure prev and next slides stick out
